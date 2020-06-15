601454a349dee58dcd7a583e07862fca088cac41
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-06-15 16:48:23 +0300

    corrected php port

--- pipline.ps1
+++ pipline.ps1
@@ -1,10 +1,10 @@
 # Build the Secret
-docker build ./dev_symfony -f ./dev_symfony/config.Dockerfile -t gcr.io/project/config:latest
+docker build ./dev_symfony -f ./dev_symfony/config.Dockerfile -t gcr.io/tourists-280412/config:latest
 
 # Build the server container, it holds the project files
-docker build ./dev_symfony -f ./dev_symfony/server.Dockerfile -t gcr.io/project/server-container:latest
-docker push gcr.io/project/server-container
+docker build ./dev_symfony -f ./dev_symfony/server.Dockerfile -t gcr.io/tourists-280412/server-container:latest
+docker push gcr.io/tourists-280412/server-container
 
 # Build the 
-docker build ./nginx -t gcr.io/project/tourists-server:latest
-docker push gcr.io/project/tourists-server
+docker build ./nginx -t gcr.io/tourists-280412/tourists-server:latest
+docker push gcr.io/tourists-280412/tourists-server

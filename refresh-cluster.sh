151c7c548c10242128dcb5f50453a449467eb655
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-07-07 11:50:40 +0300

    fixed push image title

--- refresh-cluster.sh
+++ refresh-cluster.sh
@@ -2,4 +2,4 @@ docker build ./dev_symfony -f ./dev_symfony/server.Dockerfile -t gcr.io/tourists
 docker push gcr.io/tourists-280412/server-container:latest
 
 docker build ./nginx -t gcr.io/tourists-280412/tourists-server:latest
-docker push gcr.io/tourists-280412/server-container:latest
+docker push gcr.io/tourists-280412/tourists-server:latest

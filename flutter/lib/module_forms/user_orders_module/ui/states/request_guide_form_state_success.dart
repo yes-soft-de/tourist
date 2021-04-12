62d26946d89a5619c8e23c1c178e255277f6a092
yazan99sh <syria.yazan19@gmail.com>
2021-04-13 00:31:25 +0300

    redesign success guid request

--- flutter/lib/module_forms/user_orders_module/ui/states/request_guide_form_state_success.dart
+++ flutter/lib/module_forms/user_orders_module/ui/states/request_guide_form_state_success.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:tourists/generated/l10n.dart';
 import 'package:tourists/module_forms/user_orders_module/ui/screens/request_guide/request_guide_screen.dart';
 import 'package:tourists/module_forms/user_orders_module/ui/states/request_guide_form.dart';
 import 'package:tourists/module_orders/orders_routes.dart';
@@ -11,18 +12,36 @@ class RequestGuideFormStateSuccess extends RequestGuideFormState {
   Widget getUI(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(
-          'Request Success!',
-          textAlign: TextAlign.center,
+        Center(
+          child: Icon(
+              Icons.check_circle,
+                color: Color(0xff05F29B),
+                size: 125,
+              ),
         ),
-        IconButton(
-            icon: Icon(Icons.send),
-            onPressed: () {
-              Navigator.of(context)
-                  .pushReplacementNamed(OrdersRoutes.ordersList);
-            }),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Center(
+            child: Text(
+              'Request Success!',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top:25.0,right: 8,left: 8),
+          child: Center(
+            child: FlatButton(
+              color: Colors.grey[300],
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed(OrdersRoutes.ordersList);
+              },
+              child: Text('${S.of(context).next}',),
+            ),
+          ),
+        ),
       ],
     );
   }
